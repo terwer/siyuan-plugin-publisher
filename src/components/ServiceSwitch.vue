796195c033fd75b4ab29ae12ffee6ab2d5dee07b
terwer <youweics@163.com>
2022-07-28 18:22:18 +0800

    feat:#9 发布平台开关，按需启用-属性定义与监听

--- src/components/ServiceSwitch.vue
+++ src/components/ServiceSwitch.vue
@@ -26,7 +26,7 @@
 import {ref} from 'vue'
 import {useI18n} from "vue-i18n";
 import {setConf, getConf} from "../lib/config";
-import {SWITCH_CONSTSNTS} from "../constants/switchConstants";
+import SWITCH_CONSTANTS from "../constants/switchConstants";
 
 const {t} = useI18n()
 let vuepressEnabled = ref(true)
@@ -36,28 +36,28 @@ const cnblogsEnabled = ref(false)
 const wordpressEnabled = ref(false)
 
 const jvueOnChange = (val: boolean) => {
-  setConf(SWITCH_CONSTSNTS.SWITCH_JVUE_KEY, val)
+  setConf(SWITCH_CONSTANTS.SWITCH_JVUE_KEY, val)
 }
 const confOnChange = (val: any) => {
-  setConf(SWITCH_CONSTSNTS.SWITCH_CONF_KEY, val)
+  setConf(SWITCH_CONSTANTS.SWITCH_CONF_KEY, val)
 }
 const cnblogsOnChange = (val: any) => {
-  setConf(SWITCH_CONSTSNTS.SWITCH_CNBLOGS_KEY, val)
+  setConf(SWITCH_CONSTANTS.SWITCH_CNBLOGS_KEY, val)
 }
 const wordpressOnChange = (val: any) => {
-  setConf(SWITCH_CONSTSNTS.SWITCH_WORDPRESS_KEY, val)
+  setConf(SWITCH_CONSTANTS.SWITCH_WORDPRESS_KEY, val)
 }
 
 const initConf = () => {
-  vuepressEnabled.value = getConf(SWITCH_CONSTSNTS.SWITCH_VUEPRESS_KEY) || false
-  jvueEnabled.value = getConf(SWITCH_CONSTSNTS.SWITCH_JVUE_KEY) || false
-  confEnabled.value = getConf(SWITCH_CONSTSNTS.SWITCH_CONF_KEY) || false
-  cnblogsEnabled.value = getConf(SWITCH_CONSTSNTS.SWITCH_CNBLOGS_KEY) || false
-  wordpressEnabled.value = getConf(SWITCH_CONSTSNTS.SWITCH_WORDPRESS_KEY) || false
+  vuepressEnabled.value = getConf(SWITCH_CONSTANTS.SWITCH_VUEPRESS_KEY) || false
+  jvueEnabled.value = getConf(SWITCH_CONSTANTS.SWITCH_JVUE_KEY) || false
+  confEnabled.value = getConf(SWITCH_CONSTANTS.SWITCH_CONF_KEY) || false
+  cnblogsEnabled.value = getConf(SWITCH_CONSTANTS.SWITCH_CNBLOGS_KEY) || false
+  wordpressEnabled.value = getConf(SWITCH_CONSTANTS.SWITCH_WORDPRESS_KEY) || false
 }
 
 // 默认选中vuepress
-setConf(SWITCH_CONSTSNTS.SWITCH_VUEPRESS_KEY, true)
+setConf(SWITCH_CONSTANTS.SWITCH_VUEPRESS_KEY, true)
 // 初始化
 initConf()
 </script>
--- src/constants/switchConstants.ts
+++ src/constants/switchConstants.ts
@@ -4,10 +4,12 @@ const SWITCH_CONF_KEY = "switch-conf";
 const SWITCH_CNBLOGS_KEY = "switch-cnblogs";
 const SWITCH_WORDPRESS_KEY = "switch-wordpress";
 
-export const SWITCH_CONSTSNTS = {
+const SWITCH_CONSTSNTS = {
     SWITCH_VUEPRESS_KEY,
     SWITCH_JVUE_KEY,
     SWITCH_CONF_KEY,
     SWITCH_CNBLOGS_KEY,
     SWITCH_WORDPRESS_KEY
 }
+
+export default SWITCH_CONSTSNTS
