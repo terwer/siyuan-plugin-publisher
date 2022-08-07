057a8080832a085a0dd539a10e2aad1388d85531
terwer <youweics@163.com>
2022-08-07 22:28:16 +0800

    feat:#24 非挂件服务器版-权限验证

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <publish-service/>
+  <publish-service v-if="isAuth"/>
+  <no-auth v-else/>
 </template>
 
 <script lang="ts" setup>
-// import {onMounted} from "vue";
+import {ref, onMounted} from "vue";
+import {getEnv} from "./lib/envUtil";
 // import {getNotebooks} from "./lib/siyuan/siyuanUtil";
-// import log from "./lib/logUtil";
-//
-// // 测试代码，移除
-// onMounted(async () => {
-//   const notebooks = await getNotebooks()
-//   log.logWarn("notebooks=>")
-//   log.logWarn(notebooks)
-//
-//   log.logWarn("MODE=>", import.meta.env.MODE)
-//   log.logInfo("App setup")
-// })
+import log from "./lib/logUtil";
+import {getQueryString} from "./lib/util";
+
+const isAuth = ref(false)
+
+onMounted(async () => {
+  // const notebooks = await getNotebooks()
+  // log.logWarn("notebooks=>")
+  // log.logWarn(notebooks)
+
+  const optPwd = getEnv("VITE_OPT_PWD") || ""
+  const pwd = getQueryString("pwd") || ""
+  if (pwd != "" && pwd == optPwd) {
+    isAuth.value = true
+  }
+
+  log.logWarn("MODE=>", import.meta.env.MODE)
+  log.logInfo("App setup")
+})
 </script>
 
 <script lang="ts">
 import PublishService from "./components/PublishService.vue";
+import NoAuth from "./components/NoAuth.vue";
 
 export default {
   name: 'App',
-  components: {PublishService}
+  components: {PublishService, NoAuth}
 }
 </script>
