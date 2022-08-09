043a549a181c5f79fd7a1a6febce4d1e9bd2bb57
terwer <youweics@163.com>
2022-08-09 19:11:16 +0800

    feat:#18 实现metaweblog-api客户端-迁移别名功能

---
+++ src/components/tab/main/mainadaptor/CnblogsMain.vue
@@ -0,0 +1,15 @@
+<template>
+<metaweblog-main/>
+</template>
+
+<script lang="ts">
+import MetaweblogMain from "../MetaweblogMain.vue";
+export default {
+  name: "CnblogsMain",
+  components: {MetaweblogMain}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/tab/main/mainadaptor/JVueMain.vue
@@ -0,0 +1,16 @@
+<template>
+  <metaweblog-main/>
+</template>
+
+<script>
+import MetaweblogMain from "../MetaweblogMain.vue";
+
+export default {
+  name: "JVueMain",
+  components: {MetaweblogMain}
+}
+</script>
+
+<style scoped>
+
+</style>
