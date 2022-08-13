fd98625bf2a8265899966711511a25b611d0c151
terwer <youweics@163.com>
2022-08-13 15:51:51 +0800

    feat:#18 实现metaweblog-api客户端-移除测试代码

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,13 @@ onMounted(async () => {
   log.logWarn("MODE=>", import.meta.env.MODE)
 
   // 调试模式
-  const debugMode = getBooleanEnv("VITE_DEBUG_MODE")
-  if (debugMode) {
-    log.logWarn("正在开始调试模式，请修改test/test.ts下面的test方法查看效果")
-    await test()
-    log.logWarn("测试结束")
-    return
-  }
+  // const debugMode = getBooleanEnv("VITE_DEBUG_MODE")
+  // if (debugMode) {
+  //   log.logWarn("正在开始调试模式，请修改test/test.ts下面的test方法查看效果")
+  //   await test()
+  //   log.logWarn("测试结束")
+  //   return
+  // }
 
   // 挂件模式不校验
   const widgetResult = await getWidgetId()
