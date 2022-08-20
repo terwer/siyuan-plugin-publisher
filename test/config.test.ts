5cb8acc8171d1d74f4b86fb095545948e4241654
terwer <youweics@163.com>
2022-08-20 23:52:49 +0800

    feat:#18 实现metaweblog-api客户端-localStorage测试

---
+++ test/config.test.ts
@@ -0,0 +1,24 @@
+import {assert, describe, expect, it} from 'vitest'
+import {getConf, setConf} from "../src/lib/config";
+import logUtil from "../src/lib/logUtil";
+
+describe('config test', () => {
+    it('setConf', () => {
+        const key = "test"
+        const value = "testValue"
+        setConf(key, value)
+
+        const newValue = localStorage.getItem(key)
+        logUtil.logInfo("getConf test=>")
+        logUtil.logInfo("value=>", value)
+        logUtil.logInfo("newValue=>", newValue)
+        assert(newValue, value);
+    })
+
+    it('getConf', () => {
+        const key = "test"
+        const result = getConf(key)
+        logUtil.logInfo("getConf test=>")
+        logUtil.logInfo(result)
+    })
+})
