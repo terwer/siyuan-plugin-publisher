f2598fbe8bd96b352d0499b57915deefe6ea2710
terwer <youweics@163.com>
2022-08-21 02:39:13 +0800

    feat:#18 实现metaweblog-api客户端-重写API

--- src/lib/platform/commonblog/commonblogApiAdaptor.ts
+++ src/lib/platform/commonblog/commonblogApiAdaptor.ts
@@ -1,32 +1,40 @@
 import {IApi} from "../../api";
 import {Post} from "../../common/post";
 import {UserBlog} from "../../common/userBlog";
+import {getJSONConf} from "../../config";
+import {ICommonblogCfg} from "./commonblogCfg";
 
 /**
  * 通用平台接口适配器
  */
 export class CommonblogApiAdaptor implements IApi {
-    deletePost(postid: string): Promise<boolean> {
-        return Promise.resolve(false);
+    protected cfg: ICommonblogCfg
+
+    constructor(apiType: string) {
+        this.cfg = getJSONConf<ICommonblogCfg>(apiType)
     }
 
-    editPost(postid: string, post: Post, publish?: boolean): Promise<boolean> {
-        return Promise.resolve(false);
+    public async deletePost(postid: string): Promise<boolean> {
+        throw new Error("该功能未实现，请在子类重写改方法")
     }
 
-    getPost(postid: string, useSlug?: boolean): Promise<Post> {
-        return Promise.resolve(new Post());
+    public async editPost(postid: string, post: Post, publish?: boolean): Promise<boolean> {
+        throw new Error("该功能未实现，请在子类重写改方法")
     }
 
-    getRecentPosts(numOfPosts: number, page?: number, keyword?: string): Promise<Array<Post>> {
-        return Promise.resolve([]);
+    public async getPost(postid: string, useSlug?: boolean): Promise<Post> {
+        throw new Error("该功能未实现，请在子类重写改方法")
     }
 
-    getUsersBlogs(): Promise<Array<UserBlog>> {
-        return Promise.resolve([]);
+    public async getRecentPosts(numOfPosts: number, page?: number, keyword?: string): Promise<Array<Post>> {
+        throw new Error("该功能未实现，请在子类重写改方法")
     }
 
-    newPost(post: Post, publish?: boolean): Promise<string> {
-        return Promise.resolve("");
+    public async getUsersBlogs(): Promise<Array<UserBlog>> {
+        throw new Error("该功能未实现，请在子类重写改方法")
+    }
+
+    public async newPost(post: Post, publish?: boolean): Promise<string> {
+        throw new Error("该功能未实现，请在子类重写改方法")
     }
 }
