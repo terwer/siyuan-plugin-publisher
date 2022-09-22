5399e9457e365ce5d93ed7f1b23e115c2cc5fd28
terwer <youweics@163.com>
2022-09-22 17:59:51 +0800

    feat:#85 Google插件扩展-同时兼容Google扩展与思源挂件构建，修复缺失文件

---
+++ src/pages/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <publish-service/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {onMounted} from "vue";
+import logUtil from "../lib/logUtil";
+import PublishService from "../components/PublishService.vue";
+
+onMounted(async () => {
+  logUtil.logInfo("MODE=>", import.meta.env.MODE)
+})
+</script>
+
+<script lang="ts">
+export default {
+  name: 'App'
+}
+</script>
