a99ea3af3117e7905f193973cf1176b9bf1c7c04
terwer <youweics@163.com>
2022-10-13 19:07:08 +0800

    fix: #112 UI持续优化

--- src/pages/publish/App.vue
+++ src/pages/publish/App.vue
@@ -7,7 +7,7 @@
 <script lang="ts" setup>
 import {onMounted} from "vue";
 import logUtil from "../../lib/logUtil";
-import PublishService from "../components/PublishService.vue";
+import PublishService from "../../components/PublishService.vue";
 import {isInChromeExtension} from "../../lib/chrome/ChromeUtil";
 import {getWidgetId} from "../../lib/platform/siyuan/siyuanUtil";
 
