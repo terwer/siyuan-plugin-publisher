1c30a90e27b44f013930dec293f935da0c48e45b
terwer <youweics@163.com>
2022-11-15 23:21:02 +0800

    feat: #143 浏览器插件支持Edge、Firefox、360、搜狗、猎豹

--- ext.sh
+++ ext.sh
@@ -2,8 +2,10 @@ pwd
 yarn build
 # 删除siyuan挂件专属文件
 rm ./dist/widget.json
+# 删除火狐配置
+rm ./dist/manifest-v2-for-firefox.json
 
-rm -rf ./extension/*
-mkdir extension
-cp -r ./dist/* ./extension/
+rm -rf ./extension/chrome/*
+mkdir -p extension/chrome
+cp -r ./dist/* ./extension/chrome
 echo "Chrome插件发布完毕."
---
+++ extFV2.sh
@@ -0,0 +1,12 @@
+pwd
+yarn build
+# 删除siyuan挂件专属文件
+rm ./dist/widget.json
+
+# 火狐配置
+mv ./dist/manifest-v2-for-firefox.json ./dist/manifest.json
+
+rm -rf ./extension/firefox/v2/*
+mkdir -p extension/firefox/v2
+cp -r ./dist/* ./extension/firefox/v2
+echo "Firefox V2插件发布完毕."
