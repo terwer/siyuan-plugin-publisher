f51cdafc7e132d24e9bc363484bb62282752467c
terwer <youweics@163.com>
2022-11-21 13:35:14 +0800

    chore: pretty code

--- api/index.ts
+++ api/index.ts
@@ -1,49 +1,48 @@
 import express, {Request, Response} from "express";
 import fetch from 'cross-fetch';
-import logUtil from "../src/lib/logUtil";
 
 const app = require('express')();
-const {v4} = require('uuid');
+// const {v4} = require('uuid');
 const xmlrpc = require('xmlrpc');
-const Serializer = require('xmlrpc/lib/serializer')
+// const Serializer = require('xmlrpc/lib/serializer')
 
 // 解决req.body undefined
 app.use(express.json());
 
-app.get('/api', (req: Request, res: Response) => {
-    const path = `/api/item/${v4()}`;
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
-    res.end(`Hello! Go to item: <a href="${path}">${path}</a>
-        <br /> or visit xmlrpc middleware <br />
-        <a href="/api/middleware/xmlrpc">/api/middleware/xmlrpc</a>
-    `);
-});
-
-app.get('/api/item/:slug', (req: Request, res: Response) => {
-    const {slug} = req.params;
-    res.end(`Item: ${slug}`);
-});
+// app.get('/api', (req: Request, res: Response) => {
+//     const path = `/api/item/${v4()}`;
+//     res.setHeader('Content-Type', 'text/html');
+//     res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+//     res.end(`Hello! Go to item: <a href="${path}">${path}</a>
+//         <br /> or visit xmlrpc middleware <br />
+//         <a href="/api/middleware/xmlrpc">/api/middleware/xmlrpc</a>
+//     `);
+// });
+//
+// app.get('/api/item/:slug', (req: Request, res: Response) => {
+//     const {slug} = req.params;
+//     res.end(`Item: ${slug}`);
+// });
 
 /**
  * xmlrpc请求中间代理层
  */
 app.post('/api/middleware/xmlrpc', (req: Request, res: Response) => {
-    const headers = req.headers;
+    // const headers = req.headers;
     // logUtil.logInfo(headers)
     const body = req.body
     // logUtil.logInfo(body)
 
     // 获取代理参数
-    console.log("body.fetchParams.apiUrl=>")
-    console.log(body.fetchParams.apiUrl)
-    console.log("body.fetchParams.fetchCORSParams=>")
-    console.log(body.fetchParams.fetchCORSParams)
+    // console.log("body.fetchParams.apiUrl=>")
+    // console.log(body.fetchParams.apiUrl)
+    // console.log("body.fetchParams.fetchCORSParams=>")
+    // console.log(body.fetchParams.fetchCORSParams)
 
     // =====================================
     // =====================================
     // 发送真实请求并获取结果
-    console.log("开的发送真实请求并获取结果")
+    // console.log("开始发送真实请求并获取结果")
     let client
     const xmlrpcApiUrl = body.fetchParams.apiUrl
     const xmlrpcCORSParams = body.fetchParams.fetchCORSParams
@@ -57,52 +56,52 @@ app.post('/api/middleware/xmlrpc', (req: Request, res: Response) => {
 
     let err
     try {
-        console.error("xmlrpcCORSParams.reqMethod=>")
-        console.error(xmlrpcCORSParams.reqMethod)
-        console.error("xmlrpcCORSParams.reqParams=>")
-        console.error(xmlrpcCORSParams.reqParams)
+        // console.log("xmlrpcCORSParams.reqMethod=>")
+        // console.log(xmlrpcCORSParams.reqMethod)
+        // console.log("xmlrpcCORSParams.reqParams=>")
+        // console.log(xmlrpcCORSParams.reqParams)
         const methodPromise = methodCallDirect(client, xmlrpcCORSParams.reqMethod, xmlrpcCORSParams.reqParams)
         methodPromise.then((resolve: any) => {
-            console.warn("methodPromise resolve=>")
-            console.warn(resolve)
+            // console.log("methodPromise resolve=>")
+            // console.log(resolve)
 
-            console.log("请求完成，准备返回真实结果")
+            // console.log("请求完成，准备返回真实结果")
             writeData(res, resolve)
-            console.log("请求处理已成功")
+            // console.log("请求处理已成功")
         }).catch((reason: any) => {
-            console.log("methodPromise catch=>")
-            console.log(reason)
+            // console.log("methodPromise catch=>")
+            console.error(reason)
             writeError(res, reason)
-            console.log("请求处理失败")
+            // console.log("请求处理失败")
         })
     } catch (e) {
         err = e
         console.error(e)
         writeError(res, err)
-        console.log("请求处理异常")
+        // console.log("请求处理异常")
     }
     // ========================================
     // ========================================
 });
 
 app.post('/api/middleware/fetch', (req: Request, res: Response) => {
-    const headers = req.headers;
+    // const headers = req.headers;
     // logUtil.logInfo(headers)
     const body = req.body
     // logUtil.logInfo(body)
 
     // 获取代理参数
-    console.log("body.fetchParams.apiUrl=>")
-    console.log(body.fetchParams.apiUrl)
-    console.log("body.fetchParams.fetchOptions=>")
-    console.log(body.fetchParams.fetchOptions)
-    console.log("body.fetchParams.formJson=>")
-    console.log(body.fetchParams.formJson)
+    // console.log("body.fetchParams.apiUrl=>")
+    // console.log(body.fetchParams.apiUrl)
+    // console.log("body.fetchParams.fetchOptions=>")
+    // console.log(body.fetchParams.fetchOptions)
+    // console.log("body.fetchParams.formJson=>")
+    // console.log(body.fetchParams.formJson)
 
     // =====================================
     // =====================================
     // 发送真实请求并获取结果
-    console.log("开的发送真实请求并获取结果")
+    // console.log("开始发送真实请求并获取结果")
 
     const fetchCORSApiUrl = body.fetchParams.apiUrl
     const fetchCORSOptions = body.fetchParams.fetchOptions
@@ -119,23 +118,23 @@ app.post('/api/middleware/fetch', (req: Request, res: Response) => {
     }
 
     let err
-    console.error("fetchCORS.apiUrl=>")
-    console.error(fetchCORSApiUrl)
-    console.error("fetchCORS.fetchOptions=>")
-    console.error(fetchCORSOptions)
+    // console.log("fetchCORS.apiUrl=>")
+    // console.log(fetchCORSApiUrl)
+    // console.log("fetchCORS.fetchOptions=>")
+    // console.log(fetchCORSOptions)
 
     fetch(fetchCORSApiUrl, fetchCORSOptions)
         .then((response) => {
             try {
                 response.text().then((resText) => {
-                    console.log("请求完成，准备返回真实结果")
+                    // console.log("请求完成，准备返回真实结果")
                     let resJson = {}
                     try {
                         resJson = JSON.parse(resText)
                     } catch (e) {
                         console.error(e)
                     }
-                    console.log(resJson)
+                    // console.log(resJson)
 
                     const finalRes = {
                         headers: {
@@ -144,22 +143,22 @@ app.post('/api/middleware/fetch', (req: Request, res: Response) => {
                         },
                         body: resJson
                     }
-                    console.log(finalRes)
+                    // console.log(finalRes)
                     writeStatusData(res, finalRes, response.status)
-                    console.log("请求处理已成功")
+                    // console.log("请求处理已成功")
                 })
             } catch (e) {
                 err = e
                 console.error(e)
                 writeStatusError(res, err, response.status)
-                console.log("请求处理异常")
+                // console.log("请求处理异常")
             }
         })
         .catch((reason: any) => {
-            console.log("methodPromise catch=>")
-            console.log(reason)
+            // console.log("methodPromise catch=>")
+            console.error(reason)
             writeError(res, reason)
-            console.log("请求处理失败")
+            // console.log("请求处理失败")
         })
     // ========================================
     // ========================================
@@ -226,8 +225,8 @@ async function methodCallDirect(client: any, methodName: string, params: any): P
     return new Promise(function (resolve, reject) {
         client.methodCall(methodName, params, function (error: any, data: any) {
             if (!error) {
-                console.log("resolve=>")
-                console.log(data)
+                // console.log("resolve=>")
+                // console.log(data)
                 resolve(data);
             } else {
                 reject(error);
@@ -236,5 +235,4 @@ async function methodCallDirect(client: any, methodName: string, params: any): P
     });
 }
 
-// module.exports = app;
 export default app;
