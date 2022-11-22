5464b33e4ae20139bdb322d06930728ffa36bde5
terwer <youweics@163.com>
2022-11-23 02:17:53 +0800

    fix: #119 发布到链滴，图片转换为base64

--- src/components/publish/tab/main/adaptor/common/LiandiMain.vue
+++ src/components/publish/tab/main/adaptor/common/LiandiMain.vue
@@ -1,5 +1,5 @@
 <template>
-  <CommonBlogMain :api-type="apiType" :page-id="props.pageId" :limit-rate="true" :limit-seconds="360"/>
+  <CommonBlogMain :api-type="apiType" :page-id="props.pageId" :limit-rate="true" :limit-seconds="360" :image-to-base64="true"/>
 </template>
 
 <script lang="ts" setup>
