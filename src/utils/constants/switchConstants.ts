79ca9a9aa1b37aae07d6fcb75052824bf7b83848
terwer <youweics@163.com>
2022-11-27 23:29:37 +0800

    fix: #133 支持支持Github pages、HUGO、Jekyll、Next.js content、Nuxt.js content

--- src/utils/constants/switchConstants.ts
+++ src/utils/constants/switchConstants.ts
@@ -1,8 +1,18 @@
+// Github平台
 const SWITCH_VUEPRESS_KEY = "switch-vuepress";
+const SWITCH_HUGO_KEY = "switch-hugo"
+const SWITCH_HEXO_KEY = "switch-hexo"
+const SWITCH_JEKYLL_KEY = "switch-jekyll"
+
+// Metaweblog API
 const SWITCH_JVUE_KEY = "switch-jvue";
 const SWITCH_CONF_KEY = "switch-conf";
 const SWITCH_CNBLOGS_KEY = "switch-cnblogs";
+
+// Wordpress
 const SWITCH_WORDPRESS_KEY = "switch-wordpress";
+
+// Commmon API
 const SWITCH_LIANDI_KEY = "switch-liandi";
 const SWITCH_YUQUE_KEY = "switch-yuque";
 const SWITCH_KMS_KEY = "switch-kms";
