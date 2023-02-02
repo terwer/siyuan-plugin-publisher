b60aeb4850f4c235235332ed77feff537f5b4b0b
terwer <youweics@163.com>
2023-02-02 11:52:10 +0800

    feat: 重构构建脚本-重构serve命令脚本

--- scripts/serve.py
+++ scripts/serve.py
@@ -1,1 +1,18 @@
-print("hello")
+import os
+import sys
+
+if __name__ == "__main__":
+    CWD = "./"
+    if os.getcwd().endswith("scripts"):
+        CWD = "../"
+
+    # 打印当前python版本
+    print("当前python版本:" + sys.version)
+    # 打印当前路径
+    print("当前路径:" + os.getcwd())
+
+    print("切换路径")
+    os.chdir(CWD)
+    print("当前路径:" + os.getcwd())
+
+    os.system("vercel dev --listen 6006")
