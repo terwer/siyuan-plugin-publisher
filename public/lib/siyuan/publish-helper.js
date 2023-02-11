68e55dcb4fb431f583693e3b9c3928ba9f03018d
terwer <youweics@163.com>
2023-02-11 22:26:15 +0800

    feat: #319 PicGO图形化配置界面-修复云床配置buffer读取报错问题

--- public/lib/siyuan/publish-helper.js
+++ public/lib/siyuan/publish-helper.js
@@ -243,7 +243,7 @@ const initPublishHelper = () => {
         { content: html },
         (response) => {
           window.siyuan.printWin.loadURL(response.data.url)
-          // window.siyuan.printWin.webContents.openDevTools()
+          window.siyuan.printWin.webContents.openDevTools()
         }
       )
     })
