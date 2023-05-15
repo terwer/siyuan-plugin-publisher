21a5372de30f2975456bc31039ca21efb8817d96
terwer <youweics@163.com>
2023-05-15 23:42:35 +0800

    feat: 支持复制文档ID

--- plugins/publisher-main/src/index.ts
+++ plugins/publisher-main/src/index.ts
@@ -1,13 +1,16 @@
 import { Dialog, isMobile, Menu, Plugin } from "siyuan"
-import App from "./App.svelte"
 import { DeviceDetection, DeviceTypeEnum } from "zhi-device"
 import { Env } from "zhi-env"
 import { CustomLogFactory, DefaultLogger } from "zhi-log"
 import "./index.styl"
+import PageUtil from "./utils/pageUtil"
+import Constants from "./utils/constants"
+import iconPublish from "./utils/svg"
+import HtmlUtils from "./utils/htmlUtils"
 
 const STORAGE_NAME = "menu-config"
-const SETTING_CONTAINER = "publish-tool-setting"
 
+// https://github.com/siyuan-note/siyuan/pull/8188/
 // https://github.com/vitejs/vite/issues/6582#issuecomment-1546954468
 // https://github.com/sveltejs/svelte-preprocess/issues/91#issuecomment-548527600
 export default class PublishTool extends Plugin {
@@ -31,7 +34,7 @@ export default class PublishTool extends Plugin {
   // private functions
   private _addTopBar() {
     const topBarElement = this.addTopBar({
-      icon: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/></svg>`,
+      icon: iconPublish.iconPlane,
       title: this.i18n.publishTool,
       position: "right",
       callback: () => {
@@ -52,13 +55,23 @@ export default class PublishTool extends Plugin {
       label: this.i18n.publishTo,
       submenu: [
         {
-          iconHTML: `<span class="pt-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 64C0 46.3 14.3 32 32 32c229.8 0 416 186.2 416 416c0 17.7-14.3 32-32 32s-32-14.3-32-32C384 253.6 226.4 96 32 96C14.3 96 0 81.7 0 64zM0 416a64 64 0 1 1 128 0A64 64 0 1 1 0 416zM32 160c159.1 0 288 128.9 288 288c0 17.7-14.3 32-32 32s-32-14.3-32-32c0-123.7-100.3-224-224-224c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg></span>`,
+          iconHTML: iconPublish.iconCnblogs,
           label: this.i18n.platformCnblogs,
           click: () => {
             this.logger.debug("发布到博客园")
           },
         },
       ],
+    })
+
+    // 图床
+    menu.addSeparator()
+    menu.addItem({
+      iconHTML: iconPublish.iconPicture,
+      label: this.i18n.picbed,
+      click: () => {
+        this._showPicbedDialog()
+      },
     })
 
     // 设置
@@ -69,6 +82,20 @@ export default class PublishTool extends Plugin {
       click: () => {
         this._showSettingDialog()
       },
+      submenu: [
+        {
+          label: this.i18n.settingGeneral,
+          click: () => {
+            console.log(11111)
+          },
+        },
+        {
+          label: this.i18n.settingPicbed,
+        },
+        {
+          label: this.i18n.settingPublish,
+        },
+      ],
     })
 
     // 挂件版
@@ -81,6 +108,18 @@ export default class PublishTool extends Plugin {
       },
     })
 
+    // 当前文档ID
+    const pageId = PageUtil.getPageId()
+    menu.addSeparator()
+    menu.addItem({
+      iconHTML: iconPublish.iconOl,
+      label: this.i18n.copyPageId,
+      click: async () => {
+        await HtmlUtils.copyToClipboard(pageId)
+        this.logger.info("当前文档ID已复制", pageId)
+      },
+    })
+
     // slogan
     menu.addSeparator()
     menu.addItem({
@@ -89,18 +128,6 @@ export default class PublishTool extends Plugin {
       type: "readonly",
     })
 
-    // 调试阶段显示当前文档ID
-    if (this.env.isDev()) {
-      const pageId = "112123e343242-2323"
-      menu.addSeparator()
-      menu.addItem({
-        label: pageId,
-        type: "readonly",
-        click: () => {
-          this.logger.debug("当前文档ID已复制", pageId)
-        },
-      })
-    }
     if (isMobile()) {
       menu.fullscreen()
     } else {
@@ -112,20 +139,26 @@ export default class PublishTool extends Plugin {
     }
   }
 
-  private _showSettingDialog() {
+  private _showPicbedDialog() {
     new Dialog({
-      title: `${this.i18n.setting} - ${this.i18n.publishTool}`,
-      content: `<div id="${SETTING_CONTAINER}"></div>`,
+      title: `${this.i18n.picbed} - ${this.i18n.publishTool}`,
+      content: `<div id="${PageUtil.getElementId(Constants.Page.Picbed)}"></div>`,
       width: isMobile() ? "92vw" : "520px",
     })
 
     // setting
-    new App({
-      target: document.getElementById(SETTING_CONTAINER) as HTMLElement,
-      props: {
-        url: "/setting",
-      },
+    PageUtil.createApp(Constants.Page.Picbed)
+  }
+
+  private _showSettingDialog() {
+    new Dialog({
+      title: `${this.i18n.setting} - ${this.i18n.publishTool}`,
+      content: `<div id="${PageUtil.getElementId(Constants.Page.Setting)}"></div>`,
+      width: isMobile() ? "92vw" : "520px",
     })
+
+    // setting
+    PageUtil.createApp(Constants.Page.Setting)
   }
 
   private _showPublisherDialog(publisherIndex: string) {
@@ -159,15 +192,15 @@ export default class PublishTool extends Plugin {
         const publisherBridge = new bridge.default()
         publisherBridge.init().then(() => {
           // 发布首页
-          // const pageId: any = "111111"
-          // const pageUrl = "index.html"
+          const pageId = PageUtil.getPageId()
+          const pageUrl = "index.html"
 
           // 博客首页
-          const pageId: any = undefined
-          const pageUrl = "blog/index.html"
+          // const pageId: any = undefined
+          // const pageUrl = "blog/index.html"
 
           // 详情
-          // const widgetId = "1111111"
+          // const pageId = PageUtil.getPageId()
           // const pageUrl = "detail/index.html"
 
           win.syp.renderPublishHelper(pageId, pageUrl, win, this.env.isDev())
---
+++ plugins/publisher-main/src/utils/svg.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2023, Terwer . All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Terwer designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Terwer in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Terwer, Shenzhen, Guangdong, China, youweics@163.com
+ * or visit www.terwer.space if you need additional information or have any
+ * questions.
+ */
+
+/**
+ * 图标
+ */
+const iconPublish = {
+  iconPlane: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M16.1 260.2c-22.6 12.9-20.5 47.3 3.6 57.3L160 376V479.3c0 18.1 14.6 32.7 32.7 32.7c9.7 0 18.9-4.3 25.1-11.8l62-74.3 123.9 51.6c18.9 7.9 40.8-4.5 43.9-24.7l64-416c1.9-12.1-3.4-24.3-13.5-31.2s-23.3-7.5-34-1.4l-448 256zm52.1 25.5L409.7 90.6 190.1 336l1.2 1L68.2 285.7zM403.3 425.4L236.7 355.9 450.8 116.6 403.3 425.4z"/></svg>`,
+  iconCnblogs: `<span class="pt-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 64C0 46.3 14.3 32 32 32c229.8 0 416 186.2 416 416c0 17.7-14.3 32-32 32s-32-14.3-32-32C384 253.6 226.4 96 32 96C14.3 96 0 81.7 0 64zM0 416a64 64 0 1 1 128 0A64 64 0 1 1 0 416zM32 160c159.1 0 288 128.9 288 288c0 17.7-14.3 32-32 32s-32-14.3-32-32c0-123.7-100.3-224-224-224c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg></span>`,
+  iconPicture: `<span class="pt-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 96C0 60.7 28.7 32 64 32H448c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM323.8 202.5c-4.5-6.6-11.9-10.5-19.8-10.5s-15.4 3.9-19.8 10.5l-87 127.6L170.7 297c-4.6-5.7-11.5-9-18.7-9s-14.2 3.3-18.7 9l-64 80c-5.8 7.2-6.9 17.1-2.9 25.4s12.4 13.6 21.6 13.6h96 32H424c8.9 0 17.1-4.9 21.2-12.8s3.6-17.4-1.4-24.7l-120-176zM112 192a48 48 0 1 0 0-96 48 48 0 1 0 0 96z"/></svg></span>`,
+  iconOl: `<span class="pt-icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M24 56c0-13.3 10.7-24 24-24H80c13.3 0 24 10.7 24 24V176h16c13.3 0 24 10.7 24 24s-10.7 24-24 24H40c-13.3 0-24-10.7-24-24s10.7-24 24-24H56V80H48C34.7 80 24 69.3 24 56zM86.7 341.2c-6.5-7.4-18.3-6.9-24 1.2L51.5 357.9c-7.7 10.8-22.7 13.3-33.5 5.6s-13.3-22.7-5.6-33.5l11.1-15.6c23.7-33.2 72.3-35.6 99.2-4.9c21.3 24.4 20.8 60.9-1.1 84.7L86.8 432H120c13.3 0 24 10.7 24 24s-10.7 24-24 24H32c-9.5 0-18.2-5.6-22-14.4s-2.1-18.9 4.3-25.9l72-78c5.3-5.8 5.4-14.6 .3-20.5zM224 64H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H224c-17.7 0-32-14.3-32-32s14.3-32 32-32zm0 160H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H224c-17.7 0-32-14.3-32-32s14.3-32 32-32zm0 160H480c17.7 0 32 14.3 32 32s-14.3 32-32 32H224c-17.7 0-32-14.3-32-32s14.3-32 32-32z"/></svg></span>`,
+}
+
+export default iconPublish
