da604bb6aea373dd399172c8ecbbad88a56f64a5
terwer <youweics@163.com>
2023-08-28 20:04:31 +0800

    feat: 支持Gitlab

--- src/adaptors/api/base/gitlab/commonGitlabApiAdaptor.spec.ts
+++ src/adaptors/api/base/gitlab/commonGitlabApiAdaptor.spec.ts
@@ -30,7 +30,7 @@ import { CommonGitlabApiAdaptor } from "~/src/adaptors/api/base/gitlab/commonGit
 
 describe("test commonGitlabApiAdaptor", () => {
   const appInstance = new AppInstance()
-  const gitlabCfg = new CommonGitlabConfig("terwer", "glpat-d2vTv8xEJQwmVUyw4VMr", "terwer-github-io", "main")
+  const gitlabCfg = new CommonGitlabConfig("terwer", "", "terwer-github-io", "main")
   gitlabCfg.apiUrl = "http://localhost:8002"
   gitlabCfg.home = "http://localhost:8002"
   gitlabCfg.defaultMsg = "auto published by siyuan-plugin-publisher"
--- src/adaptors/api/base/gitlab/commonGitlabApiAdaptor.ts
+++ src/adaptors/api/base/gitlab/commonGitlabApiAdaptor.ts
@@ -28,8 +28,10 @@ import { AppInstance } from "~/src/appInstance.ts"
 import { CommonGitlabConfig } from "~/src/adaptors/api/base/gitlab/commonGitlabConfig.ts"
 import { createAppLogger } from "~/src/utils/appLogger.ts"
 import { CommonGitlabClient } from "zhi-gitlab-middleware"
-import { UserBlog } from "zhi-blog-api"
-import { StrUtil } from "zhi-common"
+import { CategoryInfo, Post, UserBlog, YamlConvertAdaptor, YamlFormatObj } from "zhi-blog-api"
+import { StrUtil, YamlUtil } from "zhi-common"
+import { toRaw } from "vue"
+import { Base64 } from "js-base64"
 
 /**
  * Gitlab API 适配器
@@ -77,6 +79,106 @@ class CommonGitlabApiAdaptor extends BaseBlogApi {
     this.logger.debug("result result =>", result)
 
     return result
+  }
+
+  public async newPost(post: Post, publish?: boolean): Promise<string> {
+    this.logger.debug("start newPost =>", { post: toRaw(post) })
+    const cfg = this.cfg as CommonGitlabConfig
+
+    const filename = post.wp_slug
+    const defaultPath = post.cate_slugs?.[0] ?? cfg.blogid
+    const docPath = `${defaultPath}/${filename}.md`
+    const res = await this.gitlabClient.createRepositoryFile(docPath, post.description)
+
+    // if (!res?.content?.path) {
+    //   throw new Error("Gitlab 调用API异常")
+    // }
+    // return res.content.path
+    return ""
+  }
+
+  public async getPost(postid: string, useSlug?: boolean): Promise<Post> {
+    this.logger.debug("start getPost =>", { postid: postid })
+
+    const res = await this.gitlabClient.getRepositoryFile(postid)
+    this.logger.debug("getPost finished =>", res)
+    if (!res) {
+      throw new Error("Gitlab 调用API异常")
+    }
+
+    let commonPost = new Post()
+    commonPost.postid = res.path
+    commonPost.markdown = Base64.fromBase64(res.content)
+    commonPost.description = commonPost.markdown
+
+    // YAML属性转换
+    const yamlAdaptor: YamlConvertAdaptor = this.getYamlAdaptor()
+    if (null !== yamlAdaptor) {
+      const yamlObj = await YamlUtil.yaml2ObjAsync(commonPost.description)
+      const yamlFormatObj = new YamlFormatObj()
+      yamlFormatObj.yamlObj = yamlObj
+      this.logger.debug("extract frontFormatter, yamlFormatObj =>", yamlFormatObj)
+      commonPost = yamlAdaptor.convertToAttr(commonPost, yamlFormatObj, this.cfg)
+      this.logger.debug("handled yamlObj using YamlConverterAdaptor =>", yamlObj)
+    }
+
+    // 初始化知识空间
+    const catSlugs = []
+    const extractedPath = res.path.replace(res.name, "").replace(/\/$/, "")
+    catSlugs.push(extractedPath)
+    commonPost.cate_slugs = catSlugs
+
+    return commonPost
+  }
+
+  public async editPost(postid: string, post: Post, publish?: boolean): Promise<boolean> {
+    this.logger.debug("start editPost =>", { postid: postid, post: toRaw(post) })
+
+    // const res = await this.gitlabClient.updateRepositoryFile(post.postid, post.description)
+    // if (!res?.content?.path) {
+    //   throw new Error("Gitlab 调用API异常")
+    // }
+    return true
+  }
+
+  public async deletePost(postid: string): Promise<boolean> {
+    const res = await this.gitlabClient.deleteRepositoryFile(postid)
+    // if (!res?.commit?.sha) {
+    //   throw new Error("Gitlab 调用API异常")
+    // }
+    return true
+  }
+
+  public async getCategories(): Promise<CategoryInfo[]> {
+    return Promise.resolve([])
+  }
+
+  public async getCategoryTreeNodes(docPath: string): Promise<any[]> {
+    const res = await this.gitlabClient.getRepositoryTree(docPath)
+    return res
+  }
+
+  public async getPreviewUrl(postid: string): Promise<string> {
+    const cfg = this.cfg as CommonGitlabConfig
+    let previewUrl: string
+    previewUrl = cfg.previewUrl
+      .replace("[user]", cfg.username)
+      .replace("[repo]", cfg.githubRepo)
+      .replace("[branch]", cfg.githubBranch)
+      .replace("[docpath]", postid)
+    // 路径组合
+    previewUrl = StrUtil.pathJoin(this.cfg.home, previewUrl)
+    return previewUrl
+  }
+
+  public async getPostPreviewUrl(postid: string): Promise<string> {
+    let previewUrl: string
+    const newPostid = postid.substring(postid.lastIndexOf("/") + 1).replace(".md", "")
+    previewUrl = this.cfg.previewUrl.replace("[postid]", newPostid)
+    // 路径组合
+    previewUrl = StrUtil.pathJoin(StrUtil.pathJoin(this.cfg.home, this.cfg.username), previewUrl)
+
+    return previewUrl
   }
 }
 
