fa499c1a78469c6e01696e17fe688a463bdd9c86
terwer <youweics@163.com>
2023-10-12 22:51:34 +0800

    fix: 不分割js代码，有缓存的情况下性能更好

--- vite.config.ts
+++ vite.config.ts
@@ -237,22 +237,22 @@ export default defineConfig({
         chunkFileNames: "chunks/chunk.[name].js",
         entryFileNames: "entry.[name].js",
         assetFileNames: "assets/[name].[ext]",
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            let arr = id.toString().split("node_modules/")[1].split("/")
-            // pnpm单独处理
-            if (id.includes(".pnpm")) {
-              arr = id.toString().split(".pnpm/")[1].split("/")
-            }
-            const dep = arr[0].split("@")[0].replace(/\./g, "-")
-            // console.log("id=>", id)
-            // console.log("dep=>", dep)
-            if (dep !== "") {
-              return "vendor_" + dep
-            }
-            return "vendor"
-          }
-        },
+        // manualChunks(id) {
+        //   if (id.includes("node_modules")) {
+        //     let arr = id.toString().split("node_modules/")[1].split("/")
+        //     // pnpm单独处理
+        //     if (id.includes(".pnpm")) {
+        //       arr = id.toString().split(".pnpm/")[1].split("/")
+        //     }
+        //     const dep = arr[0].split("@")[0].replace(/\./g, "-")
+        //     // console.log("id=>", id)
+        //     // console.log("dep=>", dep)
+        //     if (dep !== "") {
+        //       return "vendor_" + dep
+        //     }
+        //     return "vendor"
+        //   }
+        // },
       },
     },
   },
