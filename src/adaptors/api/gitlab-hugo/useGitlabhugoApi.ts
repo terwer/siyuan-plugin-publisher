2df312aacc7a2e15aab5dba8f63b26742ef0f5b8
terwer <youweics@163.com>
2024-12-02 16:49:10 +0800

    fix: #1495 markdown file name support all github and git lab platforms

--- src/adaptors/api/gitlab-hugo/useGitlabhugoApi.ts
+++ src/adaptors/api/gitlab-hugo/useGitlabhugoApi.ts
@@ -64,6 +64,7 @@ const useGitlabhugoApi = async (key: string, newCfg?: GitlabhugoConfig) => {
       const githubBranch = Utils.emptyOrDefault(process.env.VITE_GITLAB_BRANCH, "main")
       const middlewareUrl = Utils.emptyOrDefault(process.env.VITE_MIDDLEWARE_URL, LEGENCY_SHARED_PROXT_MIDDLEWARE)
       cfg = new GitlabhugoConfig(githubUsername, githubAuthToken, githubRepo, githubBranch, middlewareUrl)
+      cfg.mdFilenameRule = "[slug].md"
       logger.info("Configuration is empty, using default environment variables.")
     } else {
       logger.info("Using configuration from settings...")
@@ -76,7 +77,8 @@ const useGitlabhugoApi = async (key: string, newCfg?: GitlabhugoConfig) => {
   }
 
   // 文件规则
-  cfg.mdFilenameRule = "[slug].md"
+  // 推荐别名。但是不强制使用
+  // cfg.mdFilenameRule = "[slug].md"
   // 标签
   cfg.tagEnabled = true
   // 分类
--- src/adaptors/api/vuepress2/useVuepress2Api.ts
+++ src/adaptors/api/vuepress2/useVuepress2Api.ts
@@ -64,6 +64,7 @@ const useVuepress2Api = async (key: string, newCfg?: Vuepress2Config) => {
       const githubBranch = Utils.emptyOrDefault(process.env.VITE_GITHUB_BRANCH, "main")
       const middlewareUrl = Utils.emptyOrDefault(process.env.VITE_MIDDLEWARE_URL, LEGENCY_SHARED_PROXT_MIDDLEWARE)
       cfg = new Vuepress2Config(githubUsername, githubAuthToken, githubRepo, githubBranch, middlewareUrl)
+      cfg.mdFilenameRule = "[slug].md"
       logger.info("Configuration is empty, using default environment variables.")
     } else {
       logger.info("Using configuration from settings...")
@@ -76,7 +77,8 @@ const useVuepress2Api = async (key: string, newCfg?: Vuepress2Config) => {
   }
 
   // 文件规则，占位符
-  cfg.mdFilenameRule = "[slug].md"
+  // 推荐别名。但是不强制使用
+  // cfg.mdFilenameRule = "[slug].md"
   // 标签
   cfg.tagEnabled = true
   // 分类
