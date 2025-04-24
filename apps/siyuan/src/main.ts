031a8ed8e7be6c20b3772f896dbee7b76f56eeff
terwer <youweics@163.com>
2025-04-24 18:59:25 +0800

    chore: revert peoject

--- apps/siyuan/src/main.ts
+++ apps/siyuan/src/main.ts
@@ -1,12 +1,5 @@
-/*
- *            GNU GENERAL PUBLIC LICENSE
- *               Version 3, 29 June 2007
- *
- *  Copyright (C) 2025 Terwer, Inc. <https://terwer.space/>
- *  Everyone is permitted to copy and distribute verbatim copies
- *  of this license document, but changing it is not allowed.
- */
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
 
-import { createBootStrap } from "./bootstrap.ts"
-
-createBootStrap({}, "#root")
+createApp(App).mount('#app')
--- apps/siyuan/src/vite-env.d.ts
+++ apps/siyuan/src/vite-env.d.ts
@@ -1,11 +1,1 @@
-/*
- *            GNU GENERAL PUBLIC LICENSE
- *               Version 3, 29 June 2007
- *
- *  Copyright (C) 2025 Terwer, Inc. <https://terwer.space/>
- *  Everyone is permitted to copy and distribute verbatim copies
- *  of this license document, but changing it is not allowed.
- */
-
 /// <reference types="vite/client" />
-declare module "*.vue"
